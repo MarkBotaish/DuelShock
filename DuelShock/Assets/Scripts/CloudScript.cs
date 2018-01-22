b140f718cbc7ed38feb88ca8a399b731a31889d8
MarkBotaish <markbotaish@gmail.com>
2018-01-22 18:54:31 -0500

    Added map

---
+++ DuelShock/Assets/Scripts/CloudScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color -= new Color(0.0f,0.25f,0.25f,0.0f);
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color += new Color(0.0f, 0.25f, 0.25f, 0.0f);
+    }
+}
---
+++ DuelShock/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    public float boardOneXPos;
+    public float boardOneYPos;
+    public float boardTwoXPos;
+    public float boardTwoYPos;
+
+    public int numberOfRows;
+    public int numberOfCols;
+
+    public GameObject cloudPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        buildBoard();
+    }
+
+    void buildBoard()
+    {
+        for (int i = 0; i < numberOfCols; i++)
+        {
+            for (int j = 0; j < numberOfRows; j++)
+            {
+                Instantiate(cloudPrefab, new Vector2(boardOneXPos, boardOneYPos), Quaternion.identity);
+                Instantiate(cloudPrefab, new Vector2(boardTwoXPos, boardTwoYPos), Quaternion.identity);
+                boardOneXPos++;
+                boardTwoXPos++;
+            }
+            boardOneXPos -= numberOfRows;
+            boardTwoXPos -= numberOfRows;
+            boardOneYPos--;
+            boardTwoYPos--;
+        }
+    }
+}
